ea4c1051b06078ca563f00bd300764175487ff71
emanelhos <132705021+emanelhos@users.noreply.github.com>
2023-12-23 19:44:16 +0200

    Add files via upload

---
+++ project.js
@@ -0,0 +1,28 @@
+<script>
+const body= document.querySelector('body');
+const toggle= document.getElementById('toggle');
+toggle.oneclick= Function(){
+	toggle.classList.toggle('active');
+	toggle.classList.toggle('active');
+}
+</script>
+
+<script>
+const Logintext=document.querySelector("Form.Login");
+const Loginbtn=document.querySelector("label.Login");
+const signup-Link=document.querySelector("Form.signup-link a");
+signbtn.onclick=(()=>{loginform.Style.marginLeft="-50%";
+Logintext.Style.marginLeft="-50%";
+});
+Loginbtn.onclick=(()=>{
+	loginform.Style.marginLeft="0%";
+	Logintext.Style.marginLeft="0%";
+});
+
+signuplink.onclick=(()=>{
+	signBtn.click();
+	return false; 
+});
+
+</script>
+
